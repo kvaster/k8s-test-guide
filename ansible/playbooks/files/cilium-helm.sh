04e576b1223088c1b2366c5b9ae1f91dd5fced90
Viktor Kuzmin <kvaster@gmail.com>
2020-11-24 23:18:49 +0300

    Cilium version bump to 1.9.0

--- ansible/playbooks/files/cilium-helm.sh
+++ ansible/playbooks/files/cilium-helm.sh
@@ -2,43 +2,43 @@
 
 API_SERVER_IP=10.118.12.100
 API_SERVER_PORT=6443
-CILIUM_VERSION=1.8.4
+CILIUM_VERSION=1.9.0
 CILIUM_IF=wan1
 TMP=cilium
 
-CILIUM_COMPAT_VERSION=1.8.1
+CILIUM_COMPAT_VERSION=1.8.4
 
 # no kube-proxy
 NO_KUBE_PROXY="
---set global.nodePort.enabled=true
---set global.k8sServiceHost=$API_SERVER_IP
---set global.k8sServicePort=$API_SERVER_PORT
---set global.kubeProxyReplacement=strict
---set global.nativeRoutingCIDR=10.244.0.0/16
---set global.masquerade=true
+--set nodePort.enabled=true
+--set k8sServiceHost=$API_SERVER_IP
+--set k8sServicePort=$API_SERVER_PORT
+--set kubeProxyReplacement=strict
+--set nativeRoutingCIDR=10.244.0.0/16
+--set masquerade=true
 "
 
 NO_BPF_MASQ="
---set config.bpfMasquerade=false
---set global.devices=${CILIUM_IF}
+--set bpf.masquerade=false
+--set devices=${CILIUM_IF}
 "
 
 # host reachable services
 HOST_REACHABLE="
---set global.hostServices.enabled=true
---set global.externalIPs.enabled=true
+--set hostServices.enabled=true
+--set externalIPs.enabled=true
 "
 
 # direct routing
 DIRECT_ROUTING="
---set global.tunnel=disabled
---set global.autoDirectNodeRoutes=true
---set global.nodePort.directRoutingDevice=${CILIUM_IF}
+--set tunnel=disabled
+--set autoDirectNodeRoutes=true
+--set nodePort.directRoutingDevice=${CILIUM_IF}
 "
 
 # dsr or not dsr
 DSR="
---set global.nodePort.mode=dsr
+--set loadBalancer.mode=dsr
 "
 
 OPTS="--namespace kube-system --set global.tag=v${CILIUM_VERSION}"
@@ -51,12 +51,12 @@ OPTS="${OPTS} ${DSR}"
 FILE=cilium.yml
 
 if [ "$1" == "preflight" ]; then
-  OPTS="${OPTS} --set preflight.enabled=true --set agent.enabled=false --set config.enabled=false --set operator.enabled=false"
+  OPTS="${OPTS} --set preflight.enabled=true --set agent=false --set --set operator.enabled=false"
   FILE=cilium-preflight.yml
 fi
 
 if [ "$1" == "compat" ]; then
-  OPTS="${OPTS} --set config.upgradeCompatibility=${CILIUM_COMPAT_VERSION} --set agent.keepDeprecatedProbes=true"
+  OPTS="${OPTS} --set upgradeCompatibility=${CILIUM_COMPAT_VERSION}"
 fi
 
 echo helm template cilium cilium/cilium --version ${CILIUM_VERSION} ${OPTS}
