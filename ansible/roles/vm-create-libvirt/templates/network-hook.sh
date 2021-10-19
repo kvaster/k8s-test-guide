2e19f210cb0313985f5092527386124053713da5
Viktor Kuzmin <kvaster@gmail.com>
2021-10-19 19:05:13 +0300

    Reworked libirt support. Now libvirt configures machines exactly like on hetzner. Lot's of refactoring...

--- ansible/roles/vm-create-libvirt/templates/network-hook.sh
+++ ansible/roles/vm-create-libvirt/templates/network-hook.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-vlan={{ if_list[1].name | regex_replace('(.*)\.(.*)', '\2') }}
+vlan={{ vlan_tag }}
 
 if [[ "$1" == "k8s-1" ]] && [[ "$2" == "started" ]]; then
   iface=$(cat - | xmllint --nocdata --xpath "string(/hookData/network/bridge/@name)" -)
@@ -8,4 +8,5 @@ if [[ "$1" == "k8s-1" ]] && [[ "$2" == "started" ]]; then
   ip addr add {{ subnet_ext }}.1/24 dev ${iface}.${vlan}
   ip link set dev ${iface}.${vlan} up
   ip route add {{ subnet_virt }}.0/24 via {{ subnet_ext }}.1 dev ${iface}.${vlan}
+  ip route add {{ subnet_lan }}.100 dev ${iface}.${vlan}
 fi
