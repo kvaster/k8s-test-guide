e679da7a2526b39624b7ba9a3e691d8ccea96bbb
Viktor Kuzmin <kvaster@gmail.com>
2022-04-17 21:08:44 +0300

    Use yaml insteadof yml

--- ansible/playbooks/templates/helpers/kube-setup.sh
+++ ansible/playbooks/templates/helpers/kube-setup.sh
@@ -5,7 +5,7 @@ rc-update add keepalived
 
 rc-update add kubelet
 
-kubeadm init --config kubeadm.yml --skip-phases=addon/kube-proxy --upload-certs
+kubeadm init --config kubeadm.yaml --skip-phases=addon/kube-proxy --upload-certs
 mkdir -p ~/.kube
 cp /etc/kubernetes/admin.conf ~/.kube/config
 
