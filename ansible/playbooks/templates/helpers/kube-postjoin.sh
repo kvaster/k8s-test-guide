b9d4770bac098d1d7072eae522c0e48e9bc5dfbb
Viktor Kuzmin <kvaster@gmail.com>
2024-05-24 15:51:20 +0300

    Cilium config is outdated and should be provided in separate way

--- ansible/playbooks/templates/helpers/kube-postjoin.sh
+++ ansible/playbooks/templates/helpers/kube-postjoin.sh
@@ -4,7 +4,6 @@ mkdir -p ~/.kube
 cp /etc/kubernetes/admin.conf ~/.kube/config
 
 kubectl taint nodes $(hostname) node-role.kubernetes.io/control-plane:NoSchedule-
-kubectl taint nodes $(hostname) node-role.kubernetes.io/master:NoSchedule-
 
 /etc/init.d/keepalived start
 rc-update add keepalived
--- ansible/playbooks/templates/helpers/kube-setup.sh
+++ ansible/playbooks/templates/helpers/kube-setup.sh
@@ -5,9 +5,8 @@ rc-update add keepalived
 
 rc-update add kubelet
 
-kubeadm init --config kubeadm.yaml --skip-phases=addon/kube-proxy --upload-certs
+kubeadm init --config kubeadm.yaml --skip-phases=addon/kube-proxy --upload-certs --patches /etc/kubernetes/patches
 mkdir -p ~/.kube
 cp /etc/kubernetes/admin.conf ~/.kube/config
 
 kubectl taint nodes $(hostname) node-role.kubernetes.io/control-plane:NoSchedule-
-kubectl taint nodes $(hostname) node-role.kubernetes.io/master:NoSchedule-
